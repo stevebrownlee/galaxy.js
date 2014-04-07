0b0821fc013b8fb9e5d5743333d45dd34570dc9c
chortlehoort <chortlehoort@gmail.com>
2014-04-07 16:29:48 -0500

    Removing simple http server, and now docs give instructions on installing and using https://github.com/nodeapps/http-server instead.  Simpler that way.

--- README.md
+++ README.md
@@ -1,53 +1,81 @@
 #galaxy.js
 
-##A lightweight library for building modular applications
+##A lightweight library for building modular browser applications with Require and Knockout
 
 galaxy.js was written to allow [Knockout][1] developers to inject HTML template fragments into the DOM on an as-needed basis. So instead of having all of the DOM elements downloaded to every client, only the bare minimum is sent to each client on the initial request, and as more views are accessed, only then does the additional DOM get downloaded and injected into the current document.
 
 galaxy.js was born from the stellar nursery of [stackd.io](http://www.stackd.io/). It is a utility I wrote that allows people writing single page applications (SPAs) with Knockout and [Require][2] to have a lightweight solution that gives them a modular application with DOM binding and ad-hoc DOM injection. It also utilizes the [Q](https://github.com/kriskowal/q) library to implement Promises. For it's event system, I chose [Postal.js](https://github.com/postaljs/postal.js) because it combines just the right amount of features while still having a small footprint.
 
-#Getting started
-
-## Running the example application
+# Running the example application
 In the `example` directory, I put together a very quick SPA that displays four different views, each with its own view model, with some minimal data binding.
 
-After you clone the repo, you can serve up the application by first installing Node.js if you don't have it.
+## Clone the repo
+
+    mkdir ~/projects && cd ~/projects
+    git clone https://github.com/chortlehoort/galaxy.js.git
+
+## Install Node if you don't have it
 
     sudo apt-get install nodejs  # For Ubuntu and its ilk
     sudo yum install nodejs      # For CentOS and its ilk
     
-This will install `npm`, the Node package manager service as well. Then you can install a basic HTTP server.
+This will also install `npm`, the Node package manager service as well. Then you can install a basic HTTP server.
 
-	sudo npm install http-server -g
+## Install a basic Node HTTP server 
+The `http-server` module is just a very basic project that will serve static files from the directory of your choice.
 
+  sudo npm install http-server -g
+
+## Serve the application
 Next, you go to the example application root folder and start the server.
 
-	cd galaxy.js/example
-	http-server ./
+  cd ~/projects/galaxy.js/example
+  http-server ./
     
 Now you can browse to https://localhost:8081 and you should see a very basic welcome message.
 
-If you inspect the current DOM, you'll notice that only the `<div id="welcome">` element contains child elements. This is because galaxy.js loaded a separate file named `views/welcome.html` and injected its contents into that element.
+If you inspect the current DOM (right-click anywhere in the view and choose `Inspect Element`), you'll notice that only the `<div id="welcome">` element contains child elements. This is because galaxy.js loaded a separate file named `views/welcome.html` and injected its contents into that element.
 
 The other elements `user-list`, `user-detail`, `location-list` and `location-detail` are still empty.
 
-Click on `Show Users` link and you'll be presented with another, very basic view with some additional links. If you inspect the DOM again, you'll see that the `user-list` element now contains child elements, and the `welcome` element is now hidden (but the content is still there).
+Click on `Show Users` link and you'll be presented with another, very basic view with some additional links. If you inspect the DOM again, you'll see that the `user-list` element now contains child elements, and the `welcome` element is now hidden, but the content is still there, ready to be shown again when you click the `Home` link.
 
 Building a modular Knockout/Require application this way allows you to build up the DOM for each view (and even child views, or widgets) in separate HTML files and only inject them as needed.
 
-##Building your own application
-###Dependencies
-First you need to install [Bower](http://bower.io/)
+## Building your own application
+### Create a project directory
+Create a new directory for your application.
+
+    mkdir ~/projects/intergalactic && cd ~/projects/intergalactic
+
+### Install [Bower](http://bower.io/)
 
     npm install -g bower
 
-Then use `bower` to pull these dependencies.
-    
-    bower init    # Follow instructions for initializing your project
-    bower install requirejs --save
-    bower install knockoutjs --save
-    bower install q --save
+While in your project's root directory, use `bower` to initialize your project.
 
+    bower init    # Follow instructions for initializing your project
+
+### Dependencies: Method 1 (copy & paste)
+To get the bare minimum needs to start working with galaxy.js, you can copy the `galaxy.js\example\app\bower.json` into your project directory and then run the following command.
+
+    cd ~/projects/intergalactic
+    cp /{path to galaxy}/galaxy.js/example/app/bower.json .
+    bower install
+
+### Dependencies: Method 2 (do it yourself)
+
+#### Install Require, Knockout, Q & Postal
+galaxy.js needs the following `bower` packages pulled. Make sure that you grab the `0.9.7` version of Q.
+
+    bower install requirejs knockoutjs q postal.js --save 
+
+#### Compile Q
+The Q library does not include a minified version in their Bower package, so I suggest you compile that project to get it.
+
+    cd bower_componenets/q && npm install & cd ../..
+    
+After running that command you should see the file `bower_components\q\q.min.js`.
 
   [1]: knockoutjs.com
   [2]: requirejs.org
---
+++ example/app/bower_components/galaxy.js/galaxy.js
@@ -0,0 +1,474 @@
+define(['q', 'knockout', 'postal'], function (Q, ko, postal) {
+    var DuplicateViewRegistrationException = function (message) {
+       this.message = message;
+       this.name = "DuplicateViewRegistrationException";
+    };
+
+    var UnregisteredViewWarning = function (message) {
+       this.message = message;
+       this.name = "UnregisteredViewWarning";
+    };
+
+    var MissingViewModelException = function (message) {
+       this.message = message;
+       this.name = "MissingViewModelException";
+    };
+
+    var MissingDOMElementException = function (message) {
+       this.message = message;
+       this.name = "MissingDOMElementException";
+    };
+
+    var ViewLoadedException = function (message) {
+       this.message = message;
+       this.name = "ViewLoadedException";
+    };
+
+    var MissingOptionException = function (message) {
+       this.message = message;
+       this.name = "MissingOptionException";
+    };
+
+    var $galaxy = function (options) {
+        var self = this;
+        self.network = postal.channel('galaxy');
+        self.viewmodelDirectory = '/app/viewmodel';
+        self.viewDirectory = '/app/view';
+        self.currentPayload = null;
+        self.federation = [];
+        self.currentLocation = null;
+        self.StarChart = self.StarCharter();
+
+    };
+
+    $galaxy.prototype.addRoute = function (pattern, vmId, hash) {
+        this.StarChart.addRoute(pattern, vmId, hash);
+    };
+
+    $galaxy.prototype.create = function (options) {
+        var self = this;
+
+        if (options) {
+            self.setOptions(options);
+        }
+
+        // Handle errors when require tries to load a view model id that is invalid
+        requirejs.onError = function (err) {
+            console.error(new MissingViewModelException('Unable to find the location of `' + self.currentLocation + '.js`. ' + err.message));
+        };
+
+        self.StarChart.scan();
+
+        // Detect when the hash changes
+        window.addEventListener('popstate', function (event) {
+            self.StarChart.scan();
+        });
+    };
+
+    $galaxy.prototype.setOptions = function (options) {
+        if (options && options.hasOwnProperty('channel') && options.channel !== '') {
+            this.network = postal.channel(options.channel);
+        }
+
+        if (options && options.hasOwnProperty('viewmodelDirectory')) {
+            this.viewmodelDirectory = options.viewmodelDirectory;
+        }
+
+        if (options && options.hasOwnProperty('viewDirectory')) {
+            this.viewDirectory = options.viewDirectory;
+        }
+    };
+
+    $galaxy.prototype.getDOMElements = function (id) {
+        var bindingType = (id.substr(0,1) === '.') ? 'class' : 'id';
+        var undecoratedDomBindingId = id.replace(/[\.#]/, '');
+        var elements = [];
+
+        if (bindingType === 'class') {
+            var byClass = document.getElementsByClassName(undecoratedDomBindingId);
+            if (byClass.length) {
+                for (var el in byClass) {
+                    if (typeof byClass[el] === 'object') {
+                        elements[elements.length] = byClass[el];
+                    }
+                }
+            }
+        } else if (bindingType === 'id') {
+            elements[elements.length] = document.getElementById(undecoratedDomBindingId);
+        }
+
+        if (elements[elements.length-1] === null) {
+            throw new MissingDOMElementException('The DOM element you specified ('+id+') for view model `'+this.currentLocation+'` was not found.')
+        }
+
+        return elements;
+    };
+
+
+    $galaxy.prototype.loadViewModel = function (id) {
+        var self = this;
+        var deferred = Q.defer();
+        var matchFound = self.federation.filter(function (model) {
+            return model.id === id;
+        })[0];
+
+        if (!matchFound) {
+            // Show warning that view model not loaded yet
+            console.warn(new UnregisteredViewWarning('Location with id `' + id + '` has not joined the federation. Attempting to join in now.'));
+
+            // Require the view model
+            require([self.viewmodelDirectory + '/' + id + '.js'], function (vm) {
+                self.join(vm);
+                deferred.resolve(vm);
+            });
+        } else {
+            deferred.resolve(matchFound);
+        }
+
+        return deferred.promise;
+    };
+
+    $galaxy.prototype.join = function (viewmodel) {
+        var self = this;
+
+        if (!viewmodel.hasOwnProperty('__joined')) {
+            // If a view model defined any children, join them first, and mark them as children
+            if (viewmodel.hasOwnProperty('children') && viewmodel.children.length > 0) {
+                viewmodel.children.map(function (child) {
+                    child.__parent = viewmodel.id;
+                });
+            }
+
+            // Add a __loaded property to keep track of which models have already been bound and loaded into DOM
+            if (!viewmodel.hasOwnProperty('__loaded')) {
+                viewmodel.__loaded = false;
+            }
+
+            // Give each model a show method that delegates to the internal join() function
+            if (!viewmodel.hasOwnProperty('show')) {
+                viewmodel.show = function () {
+                    self.render(this.id);
+                };
+            }
+
+            // Add the viewmodel to the internal registry
+            viewmodel.__joined = true;
+            self.federation.push(viewmodel);
+            this.network.publish(viewmodel.id + '.joined');
+
+        // View already joined
+        } else {
+            if (!viewmodel.hasOwnProperty('__parent')) {
+                console.warn('The view model with id `' + viewmodel.id + '` has already joined the federation.');
+            }
+        }
+    };
+
+    $galaxy.prototype.leave = function (id) {
+        var exists = _.findWhere(this.federation, {id: id});
+
+        if (exists) {
+            this.federation = _.filter(this.federation, function (vm) {
+                vm.id !== id;
+            });
+        }
+    };
+
+    $galaxy.prototype.loadTemplate = function (id) {
+        var self = this;
+        var deferred = Q.defer();
+        var viewmodel = self.federation.filter(function (vm) {
+            return vm.id === id;
+        })[0];
+
+        if (viewmodel) {
+            if (!viewmodel.__loaded) {
+                var viewTemplate = [this.viewDirectory, '/', viewmodel.templatePath].join('');
+
+                self.getDOMElements(viewmodel.domBindingId).forEach(function (el) {
+                    var xhr = new XMLHttpRequest();         // Create XHR object
+                    xhr.open('GET', viewTemplate, true);    // GET the HTML file for the view model
+                    xhr.onloadend = function (evt) {        // After it's loaded
+                        if (evt.target.status === 200 || evt.target.status === 302) {
+                            el.innerHTML = evt.target.responseText;             // Inject the HTML
+                            ko.applyBindings(viewmodel, el);                    // Bind view model to DOM
+                            viewmodel.__loaded = true;                            // Flag view model as loaded
+                            self.network.publish(viewmodel.id + '.bound');    // Notify subscribers of arrived event
+                            deferred.resolve();                                 // Resolve promise
+                        }
+                    };
+
+                    xhr.send();
+                });
+            } else {
+                this.network.publish(viewmodel.id + '.bound');
+                deferred.resolve();
+            }
+        }
+
+        return deferred.promise;
+    };
+
+    $galaxy.prototype.hideInactiveViews = function (id) {
+        var self = this;
+
+        // Capture current view and hide all others (not autoRender views)
+        self.federation.forEach(function (view) {
+            if (!view.autoRender && view.id !== id) {
+                self.getDOMElements(view.domBindingId).forEach(function (el) {
+                    el.style.display = 'none';
+                });
+            }
+        });
+    };
+
+    $galaxy.prototype.render = function (viewmodelId, payload) {
+        var self = this;
+        var currentViewModel = null;
+
+        self.loadViewModel(viewmodelId).then(function (vm) {
+            currentViewModel = vm;
+            self.hideInactiveViews(viewmodelId);
+
+            return self.loadTemplate(viewmodelId);
+        }).then(function () {
+
+            // After view is loaded, ensure it is visible
+            self.getDOMElements(currentViewModel.domBindingId).forEach(function (el) {
+                el.style.display = '';
+            });
+
+            self.network.publish(currentViewModel.id + '.docked', payload);
+
+            // Render any children views/widgets
+            if (currentViewModel.hasOwnProperty('children')) {
+                currentViewModel.children.map(function (child) {
+                    self.render(child.id);
+                });
+            }
+
+        }).fail(function (ex) {
+            console.error(ex);
+        }).catch(function (ex) {
+            console.error(ex);
+        }).finally(function () { });
+
+            // Immediately render any module marked with autoRender (usually navigation elements)
+            // if (viewmodel.hasOwnProperty('autoRender') && viewmodel.autoRender) {
+            //     self.render(viewmodel.id);
+            // }
+
+            // location.hash has a view id in it, and the current view matches it.  Render view.
+            // if (self.StarChart.currentLocation !== null && viewmodel.id === self.StarChart.currentLocation) {
+            //     self.render(viewmodel.id);
+            // }
+
+            // Nothing in the location.hash, and current view marked as default. Render view.
+            // if (self.StarChart.currentLocation === null && viewmodel.hasOwnProperty('defaultView') && viewmodel.defaultView) {
+            //     self.currentLocation = viewmodel.id;
+            //     self.render(viewmodel.id);
+            //     window.location.hash = viewmodel.id;
+            // }
+    };
+
+
+
+    $galaxy.prototype.StarCharter = function () {
+        var galaxy = this;
+
+        var StarChart = function () {
+            this.routes = [];
+            this.currentLocation = null;
+        };
+
+        StarChart.prototype.getRoutes = function () {
+            return this.routes;
+        };
+
+        StarChart.prototype.addRoute = function (pattern, vmId, hash) {
+            this.routes[this.routes.length] = {
+                pattern: pattern,
+                viewModel: vmId,
+                hash: hash
+            };
+
+            return this.routes;
+        };
+
+        StarChart.prototype.warp = function (options) {
+            var hashBuilder = [];
+            var match;
+            var targetLocation;
+
+            try {
+                // If the argument is a string, assume it's the location for transport 
+                if (options && typeof options === 'string') {
+                    targetLocation = options;
+                } else if (options && typeof options === 'object' && !options.hasOwnProperty('location')) {
+                    throw new MissingOptionException('You must provide a location property and a payload property when publishing the `transport` event with an object parameter.');
+                } else if (options && typeof options === 'object' && options.hasOwnProperty('location')) {
+                    targetLocation = options.location;
+                }
+
+                match = this.routes.filter(function (route) {
+                    return route.pattern === targetLocation;
+                })[0];
+
+                hashBuilder[hashBuilder.length] = targetLocation;   // Start building the location hash
+
+                if (options.hasOwnProperty('payload') && options.payload) {
+                    this.currentPayload = options.payload;
+                    for (var key in options.payload) {    // Add each k/v pair as a URL hash parameter
+                        var param = options.payload[key];
+                        hashBuilder[hashBuilder.length] = '&' + key + '=' + param;
+                    }
+                }
+
+                window.location.hash = hashBuilder.join('');  // Set the location hash
+            } catch (ex) {
+                console.error(ex);                
+            }
+
+            return match;
+        };
+
+        StarChart.prototype.scan = function (pattern) {
+            var self = this;
+            var moduleId, urlParamArray, currentPayload = {}, currentViewModel;
+
+            /*
+             *   Parse the location.hash to find the view id and any additional 
+             *   key/value pairs in the URL parameters to pass to the view model
+             */
+            var locationHash = location.hash.split('#')[1] || '';
+            moduleId = locationHash;
+
+            if (locationHash !== '') {
+                urlParamArray = locationHash.split('/');
+                moduleId = urlParamArray[0];
+                urlParamArray.splice(0,1);
+            }
+
+            // Find any registered routes where the module name matches
+            var matches = self.routes.filter(function (route) {
+                return route.pattern.split('/')[0] === moduleId;
+            }) || false;
+
+            // If any module name matches, filter it further to any registered
+            // routes with the same pattern length
+            if (matches) {
+                matches = matches.filter(function (route) {
+                    return locationHash.split('/').length === route.pattern.split('/').length;
+                }) || false;
+            }
+
+            // TODO: If the pattern lengths match, need to compare each segment, ignore
+            // the parameterized segments, and then each remaining segment needs to match
+            // 1:1 between the route pattern and the hash pattern.
+            
+            // Create a data payload from the parameterized segments
+            if (matches && matches.length && urlParamArray.length) {
+                var _arguments = matches[0].pattern.split('/')
+                _arguments.splice(0,1);
+
+                for (var i=0, j=_arguments.length; i<=j, arg=_arguments[i]; i+=1) {
+                    if (arg.substr(0,1) === ':') {
+                        currentPayload[arg.substr(1, arg.length-1)] = urlParamArray[i];
+                    }
+                }
+            }
+
+            console.log('matches',matches);
+            console.log('currentPayload',currentPayload);
+
+            if (matches && matches.length) {
+                galaxy.render(matches[0].viewModel, currentPayload);
+            }
+        };
+
+        return new StarChart();
+    };
+
+    $galaxy.prototype.depot = function () {
+        var Store = function () {
+            this.collection = ko.observableArray();
+            this.proxy = null;
+            this._dirty = false;
+        };
+
+        Store.prototype.populate = function (force) {
+            var deferred = Q.defer();
+            var self = this;
+
+            if (!self._dirty && self.collection().length && !force) {  // Already populated and not dirty
+                deferred.resolve();
+            } else if (!self._dirty || force) {              // Not dirty, but client forces population
+                self.proxy.load().then(function (models) {
+                    self.collection(models);
+                    deferred.resolve();
+                }).fail(function (err) {
+                    console.log('Failed to load from remote proxy. ' + err.toString());
+                    deferred.reject();
+                }).done();
+            } else {
+                deferred.reject();
+            }
+
+            return deferred.promise;
+        };
+
+        Store.prototype.isDirty = function () {
+            return this._dirty;
+        };
+
+        Store.prototype.sync = function () {
+            var self = this;
+
+            self.collection.forEach(function (type) {
+                if (type.hasOwnProperty("_local") && type._local) {
+                    self.proxy.save(type).then(function () {
+                        type._local = false;
+                    }).fail(function (err) {
+                        console.error(err);
+                    }).done();
+                }
+            });
+        };
+
+        Store.prototype.add = function (type) {
+            var self = this;
+
+            if (type instanceof Array) {
+                type.forEach(function (item) {
+                    self.collection.push(item);
+                });
+            } else {
+                self.collection.push(type);
+            }
+        };
+
+        Store.prototype.empty = function () {
+            this.collection.removeAll();
+        };
+
+        Store.prototype.remove = function (item) {
+            var filtered = this.collection().filter(function (type) {
+                return JSON.stringify(type) !== JSON.stringify(item);
+            });
+
+            this.collection(filtered);
+        };
+
+        Store.prototype.removeById = function (id) {
+            var filtered = this.collection().filter(function (type) {
+                return type.id !== id;
+            });
+
+            this.collection(filtered);
+        };
+
+        return new Store();
+    };
+
+    return new $galaxy();
+});
